00bd9fb6cb5dda9afc0a8649600f1dfd49450b76
braheezy <11700503+braheezy@users.noreply.github.com>
2025-06-02 16:24:32 +0000

    Implement rest of main process

--- src/main.zig
+++ src/main.zig
@@ -8,6 +8,10 @@ const znit_version = "0.1.0";
 var debug_allocator: std.heap.DebugAllocator(.{}) = .init;
 
 var parent_death_signal: u6 = 0;
+var kill_process_group: u6 = 0;
+const ts = std.posix.timespec{ .sec = 1, .nsec = 0 };
+const STATUS_MAX = 255;
+const STATUS_MIN = 0;
 
 const SignalConfiguration = struct {
     sig_mask: *std.posix.sigset_t,
@@ -19,7 +23,7 @@ pub fn main() void {
     const child_pid: std.posix.pid_t = 0;
 
     // These are passed to function to get an exit code back.
-    // const child_exitcode: i32 = -1; // This isn't a valid exit code, and lets us tell whether the child has exited.
+    const child_exitcode: i32 = -1; // This isn't a valid exit code, and lets us tell whether the child has exited.
 
     // Memory allocation setup
     const gpa, const is_debug = gpa: {
@@ -85,7 +89,18 @@ pub fn main() void {
     const ret_code = spawn(&child_sigconf, child_args, child_pid);
     if (ret_code != 0) std.process.exit(ret_code);
 
-    // while (true) {};
+    while (true) {
+        // Wait for one signal, and forward it
+        waitAndForwardSignal(&parent_sigset, child_pid) catch return 1;
+
+        // reap them zombies
+        reapZombies(child_pid, &child_exitcode) catch return 1;
+
+        if (child_exitcode != -1) {
+            std.log.info("Exiting: child has exited", .{});
+            return child_exitcode;
+        }
+    }
 }
 
 fn parseArgs(args: [][:0]u8) ![][:0]u8 {
@@ -240,4 +255,95 @@ fn restoreSignals(sigconf: *SignalConfiguration) !void {
         std.log.err("Restoring SIGTTOU handler failed: '{s}'", .{@errorName(err)});
         return error.RestoreSignalsFailed;
     };
+}
+
+fn waitAndForwardSignal(parent_sigset: *std.posix.sigset_t, child_pid: std.posix.pid_t) !void {
+    var sig: std.os.linux.siginfo_t = .{};
+    sigtimedwait(parent_sigset, &sig, ts) catch return 1;
+
+    // There is a signal to handle here
+    switch (sig.signo) {
+        SIG.CHLD => {
+            // Special-cased, as we don't forward SIGCHLD. Instead, we'll
+            // fallthrough to reaping processes.
+            std.log.debug("Received SIGCHLD", .{});
+            return;
+        },
+        else => {
+            std.log.debug("Passing signal: '{s}'", .{@tagName(sig.signo)});
+            // Forward anything else
+            std.posix.kill(if (kill_process_group != 0) -child_pid else child_pid, sig.signo) catch |err| {
+                if (err == error.ProcessNotFound) {
+                    std.log.warn("Child was dead when forwarding signal", .{});
+                } else {
+                    std.log.err("Unexpected error when forwarding signal: '{s}'", .{@errorName(err)});
+                    return err;
+                }
+            };
+        },
+    }
+}
+
+// ?TODO: Upstream to zig std lib
+fn sigtimedwait(set: *std.os.linux.sigset_t, info: *std.os.linux.siginfo_t, timeout: std.posix.timespec) !void {
+    switch (std.posix.errno(std.os.linux.syscall3(
+        std.os.linux.SYS.rt_sigtimedwait,
+        @intFromPtr(set),
+        @intFromPtr(info),
+        @intFromPtr(timeout),
+    ))) {
+        .SUCCESS => return,
+        .AGAIN, .INTR => return,
+        else => |err| return std.posix.unexpectedErrno(err),
+    }
+}
+
+fn reapZombies(child_pid: std.posix.pid_t, exitcode: *i32) !void {
+    while (true) {
+        const pid_result = std.posix.waitpid(-1, std.os.linux.W.NOHANG);
+        const current_pid = pid_result.pid;
+        const current_status = pid_result.status;
+
+        switch (current_pid) {
+            0 => {
+                std.log.debug("No child to reap", .{});
+            },
+            else => {
+                // A child was reaped. Check whether it's the main one. If it is, then
+                // set the exit_code, which will cause us to exit once we've reaped everyone else.
+                std.log.debug("Reaped child with pid: '{d}'", .{current_pid});
+                if (current_pid == child_pid) {
+                    if (std.os.linux.W.IFEXITED(current_status)) {
+                        // Our process exited normally.
+                        std.log.info("Main child exited normally (with status '{d}')", .{std.os.linux.W.EXITSTATUS(current_status)});
+                        exitcode.* = std.os.linux.W.EXITSTATUS(current_status);
+                    } else if (std.os.linux.W.IFSIGNALED(current_status)) {
+                        // Our process was terminated. Emulate what sh / bash
+                        // would do, which is to return 128 + signal number.
+                        std.log.info("Main child exited with signal (with signal '{s}')", .{@tagName(std.os.linux.W.TERMSIG(current_status))});
+                        exitcode.* = 128 + std.os.linux.W.TERMSIG(current_status);
+                    } else {
+                        std.log.err("Main child exited for unknown reason", .{});
+                        return error.UnknownExitStatus;
+                    }
+
+                    // Be safe, ensure the status code is indeed between 0 and 255.
+                    exitcode.* = exitcode.* % (STATUS_MAX - STATUS_MIN + 1);
+                }
+
+                continue;
+            },
+        }
+
+        break;
+    }
+}
+
+fn int32BitfieldCheckBounds(F: []const u32, i: usize) void {
+    // i is unsigned (>= 0), so only need to check i/32 < F.len
+    std.debug.assert(F.len > i / 32);
+}
+
+fn int32BitfieldTest(F: []const u32, i: usize) bool {
+    return (F[i / 32] & (u32(1) << (i % 32))) != 0;
 }
